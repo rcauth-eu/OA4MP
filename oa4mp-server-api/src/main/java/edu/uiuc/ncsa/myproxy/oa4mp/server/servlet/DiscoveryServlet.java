4a0a9d9bd2a1861612b34552860b3b89ab7c8085
Mischa Salle <msalle@nikhef.nl>
2017-02-17 13:48:10 +0100

    Bugfix: Fix issuer algorithm
    Fixing some shortcoming in the algorithm for getting the issuer from either the
    .well-known or /token endpoints:
    - Don't add port when default and remove double /
      When using http or https and using ports 80 or 443 respectively, we don't need
      to add the port. Also fix a double slash in the URL.
    - should strip off /token
      Not only .well-known should be stripped, also /token. The former is used in
      the .well-known end-point, the latter in the claims (since then the requestURI
      is ending in /token).
    - note that we need to do some clever parsing, since we can use both .well-known
      and .well-known/... while the getServletPath() would return just .well-known

--- oa4mp-server-api/src/main/java/edu/uiuc/ncsa/myproxy/oa4mp/server/servlet/DiscoveryServlet.java
+++ oa4mp-server-api/src/main/java/edu/uiuc/ncsa/myproxy/oa4mp/server/servlet/DiscoveryServlet.java
@@ -64,12 +64,36 @@ public class DiscoveryServlet extends MyProxyDelegationServlet {
         //JSPUtil.fwd(httpServletRequest, httpServletResponse, getDiscoveryPagePath());
     }
     protected static String getRequestURI(HttpServletRequest request, boolean includePort) {
-        String requestURI = request.getScheme() + "://" + request.getServerName() + (includePort?(":" + request.getServerPort()):"")  + request.getRequestURI();
-        //  String requestURI = request.getRequestURI();
+        int port=request.getServerPort();
+        String scheme=request.getScheme();
+        String requestURI;
+        // Don't add port when we use a default port
+        if ( (includePort==false) ||
+             (scheme.equals("https") && port==443) ||
+             (scheme.equals("http")  && port==80) )
+        {
+            requestURI =   scheme + "://" + request.getServerName() + request.getRequestURI();
+        } else {
+            requestURI =   scheme + "://" + request.getServerName() + ":" + port + request.getRequestURI();
+        }
+        // Strip off request.getServletPath() from requestURI.
+        // NOTE: getServletPath() is typically the endpoint as
+        // defined in web.xml, so not per se at the end of the URI.
+        String servletPath=request.getServletPath();
+        if (servletPath.length()>0 && requestURI.endsWith(servletPath)) {
+            requestURI = requestURI.substring(0, requestURI.length()-servletPath.length());
+        }
+        // Also need to strip off trailing /
         if (requestURI.endsWith("/")) {
             requestURI = requestURI.substring(0, requestURI.length() - 1);
         }
-        if (0 < requestURI.indexOf("/.well-known")) {
+        if (0 < requestURI.indexOf("/token")) {
+            // Strip off /token which is end of requestURI for getting claims
+            requestURI = requestURI.substring(0, requestURI.indexOf("/token"));
+        } else if (0 < requestURI.indexOf("/.well-known")) {
+            // Strip off /.well-known.
+            // NOTE that this would be the servletPath, but not typically at
+            // the end of the URI (e.g. followed by openid-configuration)
             requestURI = requestURI.substring(0, requestURI.indexOf("/.well-known"));
         }
         return requestURI;
