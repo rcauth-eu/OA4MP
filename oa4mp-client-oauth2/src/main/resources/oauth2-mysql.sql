b27d03b14df317e5c87f782cef15ccad90da848f
Mischa Salle <msalle@nikhef.nl>
2019-08-20 17:13:48 +0200

    Bugfix: update sql scripts to current source
    - add missing fields:
        - clients:
    	scopes, issuer, public_client, ldap, cfg
        - client_approvals:
    	status
        - transactions:
    	states
    - no need for including db name in create table statements,
      since we have a "USE oauth2"
    - replace not-working DEFAULT NULL with DEFAULT CURRENT_TIMESTAMP
      mariaDB doesn't like the DEFAULT NULL.
    - synchronize mysql and mariadb scripts which can essentially be the same.
    - also update client sql script for the default timestamp.

--- oa4mp-client-oauth2/src/main/resources/oauth2-mysql.sql
+++ oa4mp-client-oauth2/src/main/resources/oauth2-mysql.sql
@@ -1,34 +1,48 @@
 /*
-   This script will create the tables for a basic oa4mp install. Since MySQL has *no* varaible
+   This script will create the tables for a basic oa4mp install. Since MySQL has *no* variable
    support, everything is hard-coded. if you want something other than the default names and
-   then edit the file. The only thing
+   then edit the file.
+
+   Also, the default timestamp must all be CURRENT_TIME. This is crucial to do
+   or the default for MariaDB if no default is specified is then to update each
+   timestamp field on update, effectively rendering the field merely a timestamp
+   of the last update, rather than, sayd the original creation timestamp.
 */
+
+/*
+Usage: Log in as an administrator (such as root) that can create the user, if need be.
+
 CREATE USER 'oa4mp-client'@'localhost' IDENTIFIED BY 'PASSWORD';
 
-CREATE DATABASE oauth DEFAULT CHARACTER SET utf8;
-use oauth;
+Run the rest of this script.
+*/
 
-# Set permissions. The permissions must be granted in MySQL before the tables are created.
-GRANT ALL PRIVILEGES ON oauth2.assets TO 'oa4mp-client'@'localhost';
+CREATE DATABASE oauth DEFAULT CHARACTER SET utf8;
+USE oauth;
+
+
+CREATE TABLE assets (
+  identifier       VARCHAR(255) PRIMARY KEY,
+  private_key      TEXT,
+  username         TEXT,
+  redirect_uri     TEXT,
+  certificate      TEXT,
+  refresh_token    TEXT,
+  access_token     TEXT,
+  nonce            TEXT,
+  state            TEXT,
+  issuedat         TIMESTAMP,
+  refresh_lifetime BIGINT,
+  cert_req         TEXT,
+  token            TEXT,
+  creation_ts      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 
 COMMIT;
 
-create table oauth2.assets  (
-        identifier  varchar(255) Primary key,
-        private_key text,
-        username text,
-        redirect_uri text,
-        certificate text,
-        refresh_token text,
-        access_token text,
-        nonce text,
-        state text,
-        issuedat TIMESTAMP,
-        refresh_lifetime bigint,
-        cert_req text,
-        token text,
-        creation_ts TIMESTAMP
-    );
+# Set permissions.
 
-commit;
+GRANT ALL PRIVILEGES ON assets TO 'oa4mp-client'@'localhost';
 
+COMMIT;
--- oa4mp-server-admin-oauth2/src/main/resources/oauth2-mysql.sql
+++ oa4mp-server-admin-oauth2/src/main/resources/oauth2-mysql.sql
@@ -1,7 +1,12 @@
 /*
    This script will create the tables for a basic oa4mp install. Since MySQL has *no* variable
-   support, everything is hard-coded. if you want something other than the default names and 
+   support, everything is hard-coded. if you want something other than the default names and
    then edit the file.
+
+   Also, the default timestamp must all be CURRENT_TIME. This is crucial to do
+   or the default for MariaDB if no default is specified is then to update each
+   timestamp field on update, effectively rendering the field merely a timestamp
+   of the last update, rather than, sayd the original creation timestamp.
 */
 
 /*
@@ -9,11 +14,10 @@ Usage: Log in as an administrator (such as root) that can create the user, if ne
 
 CREATE USER 'oa4mp-server'@'localhost' IDENTIFIED BY 'PASSWORD';
 
-Run the rest of this script. The user must exist before permissions are granted.
+Run the rest of this script.
 */
 
-CREATE DATABASE oauth2
-DEFAULT CHARACTER SET utf8;
+CREATE DATABASE oauth2 DEFAULT CHARACTER SET utf8;
 USE oauth2;
 
 /*
@@ -22,11 +26,13 @@ Some useful commands. Lst two list users and will show permissions for a single 
  Show schemas;
  SELECT User FROM mysql.user;
  SHOW GRANTS FOR 'user'@'localhost';
- Another note: The timestamp fields are given a default value of NULL since under MariaDB the default now is to
- change the value of any timestamp field on update, effectively rendering all of the "last modified"
+ Another note: The timestamp fields are given a default value of CURRENT_TIME
+ since under MariaDB the default now is to change the value of any timestamp
+ field on update, effectively rendering all of the "last modified". Setting to
+ CURRENT_TIME unsets the other default of ON UPDATE CURRENT_TIMESTAMP
  */
 
-CREATE TABLE oauth2.clients (
+CREATE TABLE clients (
   client_id     VARCHAR(255) PRIMARY KEY,
   public_key    TEXT,
   name          TEXT,
@@ -34,25 +40,26 @@ CREATE TABLE oauth2.clients (
   error_url     TEXT,
   email         TEXT,
   proxy_limited BOOLEAN,
-  creation_ts   TIMESTAMP,
-  rt_lifetime   bigint,
+  creation_ts   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  rt_lifetime   BIGINT,
   callback_uri  TEXT,
-  sign_tokens   BOOLEAN
+  sign_tokens   BOOLEAN,
+  scopes        TEXT,
+  issuer        TEXT,
+  public_client BOOLEAN,
+  ldap          TEXT,
+  cfg           TEXT
 );
 
-
-
-
-CREATE TABLE oauth2.adminClients (
+CREATE TABLE adminClients (
   admin_id     VARCHAR(255) PRIMARY KEY,
   name         TEXT,
   secret       TEXT,
   email        TEXT,
-  creation_ts  TIMESTAMP,
+  creation_ts  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   vo           TEXT,
   issuer       TEXT
 );
-
 
 CREATE TABLE permissions (
   permission_id VARCHAR(255) PRIMARY KEY,
@@ -63,17 +70,18 @@ CREATE TABLE permissions (
   can_read      BOOLEAN,
   can_remove    BOOLEAN,
   can_write     BOOLEAN,
-  creation_ts   TIMESTAMP
+  creation_ts   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE oauth2.client_approvals (
+CREATE TABLE client_approvals (
   client_id   VARCHAR(255) PRIMARY KEY,
   approver    TEXT,
   approved    BOOLEAN,
-  approval_ts TIMESTAMP
+  approval_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  status      TEXT
 );
 
-CREATE TABLE oauth2.transactions (
+CREATE TABLE transactions (
   temp_token          VARCHAR(255) PRIMARY KEY,
   temp_token_valid    BOOLEAN,
   callback_uri        TEXT,
@@ -88,10 +96,11 @@ CREATE TABLE oauth2.transactions (
   certificate         TEXT,
   username            TEXT,
   myproxyUsername     TEXT,
-  access_token_valid tinyint(1) DEFAULT NULL,
-  auth_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  nonce text,
-  scopes text,
+  access_token_valid  BOOLEAN,
+  auth_time           TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  nonce               TEXT,
+  scopes              TEXT,
+  states              TEXT,
   UNIQUE INDEX verifier (verifier_token(255)),
   UNIQUE INDEX accessToken (access_token(255)),
   UNIQUE INDEX refreshToken (refresh_token(255))
@@ -101,10 +110,10 @@ CREATE TABLE oauth2.transactions (
 COMMIT;
 # Now to grant restricted access. The  tables have to exist before this step
 
-GRANT All  ON oauth2.client_approvals TO 'oa4mp-server'@'localhost';
-GRANT ALL  ON oauth2.clients TO 'oa4mp-server'@'localhost';
-GRANT ALL  ON oauth2.adminClients TO 'oa4mp-server'@'localhost';
-GRANT ALL  ON oauth2.transactions TO 'oa4mp-server'@'localhost';
-GRANT ALL  ON oauth2.permissions TO 'oa4mp-server'@'localhost';
+GRANT All  ON client_approvals TO 'oa4mp-server'@'localhost';
+GRANT ALL  ON clients TO 'oa4mp-server'@'localhost';
+GRANT ALL  ON adminClients TO 'oa4mp-server'@'localhost';
+GRANT ALL  ON transactions TO 'oa4mp-server'@'localhost';
+GRANT ALL  ON permissions TO 'oa4mp-server'@'localhost';
 
-commit;
+COMMIT;
